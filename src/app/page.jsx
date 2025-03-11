19f1758aafa697f4ba2d995ba08abd612f8a1377
Miguel Sarti <miguelsarti@aluno.senai.br>
2025-03-11 11:56:57 -0300

    need fix cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Sidebar from "@/components/sidebar";
 import DashboardContent from "@/components/dashboardContent";
 import Footer from "@/components/footer";
+import SummaryCards from "@/components/summaryCards";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
 
       <main className={styles.dashboardContainer}>
       <Sidebar />
+      <SummaryCards />
       <DashboardContent />
       </main>
       
