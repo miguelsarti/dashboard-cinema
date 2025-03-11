580e0e9d722a5d943bf5d945898a4dae312f4877
Miguel Sarti <miguelsarti@aluno.senai.br>
2025-03-11 09:33:31 -0300

    feat: create sidebar component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,15 @@
 import Header from "@/components/header";
 import styles from "./page.module.css";
+import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <main className={styles.dashboardContainer}>
+      <Sidebar />
+      </main>
     </div>
   );
 }
